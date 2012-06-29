nmalkin/kpi-dashboard

commit fb42d763daf9eb084a0ad2b735a45a3c26440ea6

Permit dragging of entire range in date slider

static/js/dashboard.js
@@ -780,8 +780,9 @@ $('input[type=date].end').val(LATEST_DATE);
     var startTime = dateToTimestamp(EARLIEST_DATE);
     var endTime = dateToTimestamp(LATEST_DATE);
 
-    $('.date-slider').slider({
+    $('.date-slider').dragslider({
         range: true,
+        rangeDrag: true,
         min: startTime,
         max: endTime,
         values: [startTime, endTime],

static/index.html
@@ -211,6 +211,7 @@
         <script type="text/javascript" src="js/lib/d3.js"></script>
         <script type="text/javascript" src="js/lib/sankey.js"></script>
         <script type="text/javascript" src="js/lib/rickshaw.js"></script>
+        <script type="text/javascript" src="js/lib/dragslider.js"></script>
         <script type="text/javascript" src="js/dashboard.js"></script>
     </body>
 </html>

static/js/lib/dragslider.js
@@ -1,0 +1,105 @@
+// This code was written by Omnosis and originally posted at:
+// http://stackoverflow.com/a/6095941
+
+(function( $, undefined ) {
+ 
+$.widget("ui.dragslider", $.ui.slider, {
+   
+    options: $.extend({},$.ui.slider.prototype.options,{rangeDrag:false}),
+   
+    _create: function() {
+      $.ui.slider.prototype._create.apply(this,arguments);
+      this._rangeCapture = false;
+    },
+   
+    _mouseCapture: function( event ) {
+      var o = this.options;
+ 
+      if ( o.disabled ) return false;
+   
+      if(event.target == this.range.get(0) && o.rangeDrag == true && o.range == true) {
+        this._rangeCapture = true;
+        this._rangeStart = null;
+      }
+      else {
+        this._rangeCapture = false;
+      }
+     
+      $.ui.slider.prototype._mouseCapture.apply(this,arguments);
+ 
+      if(this._rangeCapture == true) { 
+          this.handles.removeClass("ui-state-active").blur();  
+      }
+     
+      return true;
+    },
+   
+    _mouseStop: function( event ) {
+      this._rangeStart = null;
+      return $.ui.slider.prototype._mouseStop.apply(this,arguments);
+    },
+   
+    _slide: function( event, index, newVal ) {
+      if(!this._rangeCapture) {
+        return $.ui.slider.prototype._slide.apply(this,arguments);
+      }
+     
+      if(this._rangeStart == null) {
+        this._rangeStart = newVal;
+      }
+     
+      var oldValLeft = this.options.values[0],
+          oldValRight = this.options.values[1],
+          slideDist = newVal - this._rangeStart,
+          newValueLeft = oldValLeft + slideDist,
+          newValueRight = oldValRight + slideDist,
+          allowed;
+     
+      if ( this.options.values && this.options.values.length ) {
+        if(newValueRight > this._valueMax() && slideDist > 0) {
+          slideDist -= (newValueRight-this._valueMax());
+          newValueLeft = oldValLeft + slideDist;
+          newValueRight = oldValRight + slideDist;
+        }
+       
+        if(newValueLeft < this._valueMin()) {
+          slideDist += (this._valueMin()-newValueLeft);
+          newValueLeft = oldValLeft + slideDist;
+          newValueRight = oldValRight + slideDist;
+        }
+ 
+        if ( slideDist != 0 ) {
+          newValues = this.values();
+          newValues[ 0 ] = newValueLeft;
+          newValues[ 1 ] = newValueRight;
+         
+          // A slide can be canceled by returning false from the slide callback
+          allowed = this._trigger( "slide", event, {
+            handle: this.handles[ index ],
+            value: slideDist,
+            values: newValues
+          } );
+         
+          if ( allowed !== false ) {
+            this.values( 0, newValueLeft, true );
+            this.values( 1, newValueRight, true );
+          }
+          this._rangeStart = newVal;
+        }
+      }
+     
+     
+     
+    },
+   
+   
+    /*
+    //only for testing purpose
+    value: function(input) {
+        console.log("this is working!");
+        $.ui.slider.prototype.value.apply(this,arguments);
+    }
+    */
+});
+ 
+})(jQuery);

commit 93ae88c283d8f7e2c35081d2b5e71315e4fab865

Fix regression: slider not updated when date changed
introduced in fb42d76

static/js/dashboard.js
@@ -807,12 +807,12 @@ $('input[type=date]').change(function(e) {
     // Update the slider to reflect the change
     var target = $(e.target);
     var slider = report.tab.find('.date-slider');
-    var currentValues = slider.slider('option', 'values');
+    var currentValues = slider.dragslider('option', 'values');
     var timestamp = dateToTimestamp(target.val());
     if(target.hasClass('start')) {
-        slider.slider('option', 'values', [timestamp, currentValues[1]]);
+        slider.dragslider('option', 'values', [timestamp, currentValues[1]]);
     } else if(target.hasClass('end')) {
-        slider.slider('option', 'values', [currentValues[0], timestamp]);
+        slider.dragslider('option', 'values', [currentValues[0], timestamp]);
     }
 
     dateChanged(report);