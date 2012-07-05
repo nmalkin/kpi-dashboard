8676c6d698d4e8514040d7cb4bbd78d889f28b12
Nathan Malkin <nmalkin@mozilla.com>
2012-07-05 15:40:41 -0700

    Remove mock-up of visitor flow Sankey diagram

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -66,13 +66,6 @@ var _reports = {
             start: dateToTimestamp(EARLIEST_DATE),
             end: dateToTimestamp(LATEST_DATE),
             segmentation: null
-        },
-    // Report: login flow
-    flow:
-        {
-            kpi: 'flow',
-            tab: $('#flow'),
-            chart_id: '#flow-chart'
         }
 };
 
@@ -539,172 +532,6 @@ function targetReport(element) {
 }
 
 
-/*** FLOW REPORT ***/
-function makeFlowReport(report) {
-    var margin = {top: 1, right: 1, bottom: 6, left: 1},
-        width = 700 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-    var formatNumber = d3.format(",.0f"),
-        format = function(d) { return formatNumber(d) + "%"; },
-        color = d3.scale.category20();
-
-    var svg = d3.select(report.chart_id).append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var sankey = d3.sankey()
-        .nodeWidth(15)
-        .nodePadding(10)
-        .size([width, height]);
-
-    var path = sankey.link();
-
-    var data = {
-        "nodes":
-        [
-            {
-                "name": "Window opens"
-            },
-            {
-                "name": "Authenticated users"
-            },
-            {
-                "name": "Unauthenticated users"
-            },
-            {
-                "name": "Returning users"
-            },
-            {
-                "name": "New users"
-            },
-            {
-                "name": "Verified passwords"
-            },
-            {
-                "name": "Passwords created"
-            },
-            {
-                "name": "Logged in"
-            }
-        ],
-        "links":
-        [
-            // Authenticated
-            {
-                "source": 0,
-                "target": 1,
-                "value": 30
-            },
-            // Unauthenticated
-            {
-                "source": 0,
-                "target": 2,
-                "value": 70
-            },
-            // Returning, but not authenticated
-            {
-                "source": 2,
-                "target": 3,
-                "value": 30
-            },
-            // Not authenticated, new
-            {
-                "source": 2,
-                "target": 4,
-                "value": 37 // note that we lost a few: we never got their email, so we never found out if they're new or returning
-            },
-            // Returning users verifying passwords
-            {
-                "source": 3,
-                "target": 5,
-                "value": 27
-            },
-            // Users with verified passwords logged in
-            {
-                "source": 5,
-                "target": 7,
-                "value": 24
-            },
-            // Authenticated people logging in (directly)
-            {
-                "source": 1,
-                "target": 7,
-                "value": 28
-            },
-            // New users creating passwords = verification emails sent
-            {
-                "source": 4,
-                "target": 6,
-                "value": 32
-            },
-            // New users with verified emails logging in
-            {
-                "source": 6,
-                "target": 7,
-                "value": 26
-            }
-        ]
-    };
-
-  sankey
-      .nodes(data.nodes)
-      .links(data.links)
-      .layout(32);
-
-  var link = svg.append("g").selectAll(".link")
-      .data(data.links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", path)
-      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-      .sort(function(a, b) { return b.dy - a.dy; })
-      .on("dblclick", function(d) {
-          console.log('Will now open the report about the drop-off from ' + d.source.name + ' to ' + d.target.name);
-      });
-
-  link.append("title")
-      .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
-
-  var node = svg.append("g").selectAll(".node")
-      .data(data.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-    .call(d3.behavior.drag()
-      .origin(function(d) { return d; })
-      .on("dragstart", function() { this.parentNode.appendChild(this); })
-      .on("drag", dragmove));
-
-  node.append("rect")
-      .attr("height", function(d) { return d.dy; })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
-      .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
-    .append("title")
-      .text(function(d) { return d.name + "\n" + format(d.value); });
-
-  node.append("text")
-      .attr("x", -6)
-      .attr("y", function(d) { return d.dy / 2; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .attr("transform", null)
-      .text(function(d) { return d.name; })
-    .filter(function(d) { return d.x < width / 2; })
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
-
-  function dragmove(d) {
-    d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
-    sankey.relayout();
-    link.attr("d", path);
-  }
-}
-
-
 /*** ON LOAD ***/
 
 // Setup segmentation controls for all reports
@@ -779,9 +606,6 @@ getData('milestones', {}, function(data) {
         cumulativeToggled(report);
     });
 });
-
-// Set up flow report
-makeFlowReport(_reports.flow);
 
 $('.reload').click(function(e) {
     var report = targetReport(e.target);
