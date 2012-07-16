nmalkin/kpi-dashboard

commit fa809baee01fa571690277ced98a20da48d5a339

Remove redundant parameter in updateGraph

static/js/dashboard.js
@@ -332,8 +332,9 @@ function initStepGraph(report) {
  *     Note: the graph needs to be initialized for that to work.
  * @param {Array} an array of series objects (in Rickshaw format)
  */
-function updateGraph(report, newSeries) {
+function updateGraph(report) {
     // Rickshaw extends the prototype of the series array. Copy over the extension(s).
+    var newSeries = report.series;
     newSeries.active = report.graph.series.active;
 
     // Update the graph with the new series
@@ -385,7 +386,7 @@ function dateChanged(report) {
     }
 
     loadData(report, function() {
-        updateGraph(report, report.series);
+        updateGraph(report);
     });
 
     return true;
@@ -426,7 +427,7 @@ function cumulativeToggled(report) {
 
     if(cumulative) { // Load data and update graph
         loadData(report, function() {
-            updateGraph(report, report.series);
+            updateGraph(report);
         });
     } else {
         segmentationChanged(report);        
@@ -471,9 +472,11 @@ function updateDisplayedSegments(report) {
     // Rickshaw breaks on empty series, so give it a blank one instead.
     if(newSeries.length === 0) {
         return false;
+    } else {
+        report.series = newSeries;
     }
 
-    updateGraph(report, newSeries);
+    updateGraph(report);
     return true;
 }
 

commit 7a82e3a826e7ae10b7678bcb1143b3e942e82cea

Fix segments not reappearing on re-selection
After being toggled off, then toggled on, segments in the "sites logged in" and "sign-in
attempts" reports weren't shown.

The issue was occurring because, in the process of updating displayed
segments, the series in the report object were being overwritten with the
"filtered" series — the ones with the segment removed. Thus, this data
was being lost permanently.

To fix this, the full (unfiltered) series are stored in a temporary
variable before the graph is updated; then they are restored.

This regression was introduced in fa809ba.

static/js/dashboard.js
@@ -483,11 +483,18 @@ function updateDisplayedSegments(report) {
     // Rickshaw breaks on empty series, so give it a blank one instead.
     if(newSeries.length === 0) {
         return false;
-    } else {
-        report.series = newSeries;
     }
 
+    // Temporarily overwrite full series with filtered series,
+    // so that updateGraph can get at them.
+    var allSeries = report.series;
+    report.series = newSeries;
+
     updateGraph(report);
+
+    // Restore full series
+    report.series = allSeries;
+
     return true;
 }
 