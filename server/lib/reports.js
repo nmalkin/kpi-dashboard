b936089bddb9856ce2356915f602d11672647813
Nathan Malkin <nmalkin@mozilla.com>
2012-07-17 18:57:55 -0700

    Remove code deprecated by switch to database backend

--- server/lib/reports.js
+++ server/lib/reports.js
@@ -1,85 +1,15 @@
 "use strict";
 
-var aggregate = require('./aggregate'),
-    config = require('./config'),
+var config = require('./config'),
     data = require('./data'),
     db = require('./db'),
     util = require('./util');
 
-/*
- * Given an array of data points, aggregates it by date
- * @param {Array} dataArray array of data objects
- * @return {Object} the data, aggregated by the date of the timestamp
- * @see aggregate.aggregateData
- */
-function dateAggregator(dataArray) {
-    return aggregate.aggregateData(dataArray, function(datum) {
-        return [ data.getDate(datum) ];
-    });
-}
-
 /**
- * Put data into buckets based on the desired segmentation
- */
-function segmentData(rawData, segmentation) {
-    var segmentedData;
-
-    if(segmentation === null) { // If no segmentation is specified,
-        // everything goes into one bucket.
-        segmentedData = { Total: rawData };
-    } else { // Otherwise, aggregate by known segments.
-        var segments = data.getSegmentations()[segmentation];
-        segmentedData = aggregate.aggregateData(rawData, function(datum) {
-                var segment = data.getSegmentation(segmentation, datum);
-                return segments.indexOf(segment) === -1 ?
-                    [ 'Other' ] : [ segment ];
-                    // If the segment is unknown, categorize it as "other."
-            }
-        );
-    }
-
-    return segmentedData;
-}
-
-/**
- * Processes request for summarized data, aggregated by date.
- *     Handles requesting and segmenting data, all you need to provide
- *     is a way to summarize the data.
- * NOTE: none of the parameters are validated (since this is an internal interface);
- *       this should happen elsewhere
+ * Reports mean number of sites logged in
  * @param {String} segmentation segmentation type or null for none
  * @param {Integer} start Unix timestamp of start time or null for none
  * @param {Integer} end Unix timestamp of end time or null for none
- * @param {function: Array -> Object } aggregator function mapping array of data
- *     to object where each field is a bucket of dta objects
- * @param {function: Array -> Number} summarizer function saying how to reduce
- *     an array of data to a single number
- */
-function summaryReport(segmentation, start, end, aggregator, summarizer, callback) {
-    // Get the requested data
-    data.getData(start, end, function(rawData) {
-        var segmentedData = segmentData(rawData, segmentation);
-
-        for(var segment in segmentedData) { // for each segment:
-            if(segmentedData.hasOwnProperty(segment)) {
-                // Put data into buckets according to the aggregator
-                var aggregatedData = aggregator(segmentedData[segment]);
-
-                // Summarize the data:
-                var summarizedData = aggregate.summarizeData(aggregatedData, summarizer);
-
-                // Replace segmented data with summarized data
-                segmentedData[segment] = summarizedData;
-            }
-        }
-
-        callback(segmentedData);
-    });
-}
-
-/**
- * Reports median number of sites logged in
- * @see summaryReport for parameter documentation
  */
 exports.sites = function(segmentation, start, end, callback) {
     var dbOptions = {
@@ -140,7 +70,9 @@ exports.sites = function(segmentation, start, end, callback) {
 
 /**
  * Reports total number of assertions generated
- * @see summaryReport for parameter documentation
+ * @param {String} segmentation segmentation type or null for none
+ * @param {Integer} start Unix timestamp of start time or null for none
+ * @param {Integer} end Unix timestamp of end time or null for none
  */
 exports.assertions = function(segmentation, start, end, callback) {
     var dbOptions = {
@@ -189,6 +121,9 @@ exports.assertions = function(segmentation, start, end, callback) {
 
 /**
  * Reports the number of users at each step in the sign-in flow for new users
+ * @param {String} segmentation segmentation type or null for none
+ * @param {Integer} start Unix timestamp of start time or null for none
+ * @param {Integer} end Unix timestamp of end time or null for none
  */
 exports.new_user = function(segmentation, start, end, callback) {
     var dbOptions = {
@@ -246,6 +181,8 @@ exports.new_user = function(segmentation, start, end, callback) {
 
 /**
  * Reports fraction of users at each step in the new user flow, over time
+ * @param {Integer} start Unix timestamp of start time or null for none
+ * @param {Integer} end Unix timestamp of end time or null for none
  */
 exports.new_user_time = function(start, end, callback) {
     var dbOptions = {
